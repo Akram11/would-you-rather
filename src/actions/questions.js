8aeb11ff27e3f28a1da50f555c91f45b4ae32841
Akram A <akram.f11@gmail.com>
2019-06-05 21:55:11 +0200

    updated questions action

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,20 @@
+import {_getQuestions} from '../utils/_DATA'
+
 export const RECEIVE_QUESTIONS = 'REVCEIVE_QUESTIONS'
 
-export const receiveQuestions =() => ({
+export const receiveQuestions = () => ({
     type: RECEIVE_QUESTIONS,
     questions,
 })
+
+export function handleInitialQuestions(){
+    return (dispatch) =>{
+        return _getQuestions()
+        .then(({questions})=> {
+            dispatch(receiveQuestions(questions))
+        })
+    }
+}
 
 // export function receiveQuestions(){
 //     return{
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 
-export const receiveUsers =() => ({
+export const receiveUsers = () => ({
     type: RECEIVE_USERS,
     users,
 })
