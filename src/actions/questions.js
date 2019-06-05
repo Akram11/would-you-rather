b3f90f56b4536dc76cf00d0c320e7ae8441caa46
Akram A <akram.f11@gmail.com>
2019-06-05 21:23:16 +0200

    added actions files

---
+++ src/actions/questions.js
@@ -0,0 +1,13 @@
+export const RECEIVE_QUESTIONS = 'REVCEIVE_QUESTIONS'
+
+export const receiveQuestions =() => ({
+    type: RECEIVE_QUESTIONS,
+    questions,
+})
+
+// export function receiveQuestions(){
+//     return{
+//         type: RECEIVE_QUESTIONS,
+//         questions,
+//     }
+// }
