c675b1f225fd21f471f34e449b337c1c89fc2711
Akram11 <akram.f11@gmail.com>
2019-06-06 17:43:32 +0200

    added middlewares

---
+++ src/middlewares/logger.js
@@ -0,0 +1,13 @@
+import { applyMiddleware } from 'redux'
+
+const logger = store => next => action => {
+    console.group(action.type)
+    console.log('The action: ', action)
+    let result = next(action)
+    console.log('The new state', store.getState())
+    console.groupEnd()
+    return result
+}
+
+
+export default logger
