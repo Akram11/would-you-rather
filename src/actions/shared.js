a7cc38068f1721389b5bdc478b740fa1933596c5
Akram11 <akram.f11@gmail.com>
2019-06-06 17:02:09 +0200

    Added AuthedUser reducer

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,10 @@
 import {getInitialData} from '../utils/API'
 import receiveUsers from './users'
 import receiveQuestions from './questions'
+import setAuthedUser from './authedUser'
 
+
+const AUTHED_ID = 'sarahedo'
 
 export function handleInitialData(){
     return(dispatch) => {
@@ -9,6 +12,7 @@ export function handleInitialData(){
         .then(({users, questions} )=> {
             dispatch(receiveUsers(users))
             dispatch(receiveQuestions(questions))
+            dispatch(setAuthedUser(AUTHED_ID))
         })
     }
 }
