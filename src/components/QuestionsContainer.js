911436747ae071f28582f70642f2b697331c9de1
Akram11 <akram.f11@gmail.com>
2019-06-07 13:49:29 +0200

    added QuestionsContainer component

---
+++ src/components/QuestionsContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class QuestionsContainer extends Component {
+
+    render() {
+        return (
+            <div>QuestionsContainer</div>
+        )
+    }
+
+}
+
+
+export default QuestionsContainer
