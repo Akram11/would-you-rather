e70597f6ca78f9b0c34d7df83f97ff1e2d3d65d9
Akram11 <akram.f11@gmail.com>
2019-06-07 14:17:50 +0200

    connect QuestionsContainer to the store using {connect } from react-redux

--- src/components/QuestionsContainer.js
+++ src/components/QuestionsContainer.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import Question from './Question'
+import { connect } from "react-redux";
 
 class QuestionsContainer extends Component {
 
     render() {
         return (
-            <div className ="container">QuestionsContainer
+            <div className ="container">
+                QuestionsContainer
+                {this.props.questionIds.map((id) => (
+                    <li>
+                        <p>question id: {id}</p>
+                        <Question />
+                    </li>
+                ))}
             <Question />
             </div>
         )
@@ -14,4 +22,11 @@ class QuestionsContainer extends Component {
 }
 
 
-export default QuestionsContainer
+function mapStateToProps({ questions }) {
+     return {
+         questionIds: Object.keys(questions)
+     }    
+}
+
+
+export default connect(mapStateToProps)(QuestionsContainer)
