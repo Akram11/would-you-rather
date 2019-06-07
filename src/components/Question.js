137f5b534ee1ec5c09ef0fc6232e63f53af71cf6
Akram11 <akram.f11@gmail.com>
2019-06-07 19:28:34 +0200

    add variables from the helper to the Question UI

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,23 +4,24 @@ import {formatQuestion} from '../utils/helper'
 class Question extends Component {
 
     render() {
-        console.log('props:',this.props.authedUser)
+        const {question} = this.props
+        console.log(this.props)
         return (
             <div className='question'>
                 <div className = 'q-header'>
-                    USER asks: 
+                    {question.name} Asks: 
                 </div>
                 <div className = 'q-body'>
                 <div className = 'asker-info' >
                 <img src="https://img.icons8.com/color/48/000000/user.png"
                      alt = "avatar pic"
                      className = 'avatar' />
-                <span className = 'asker-name'>Asker Name</span>
+                <span className = 'asker-name'>{question.name}</span>
                 </div>
 
                 <div className = 'poll'>
                     <span> Would you rather? </span>
-                    <span> ..... do ....</span>
+                    <span> {question.text}</span>
                     <button> view poll </button>
                 </div>
                 </div>
@@ -31,9 +32,9 @@ class Question extends Component {
     }
 }
 
-function mapStateToProps({authedUser, questions}, {id}){
+function mapStateToProps({authedUser,users, questions}, {id}){
     const question = questions[id]
-    const author = question.author
+    const author = users[question.author]
 
     return{
         authedUser,
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,11 +3,13 @@ export function formatQuestion(question, author ,authedUser){
     const {id, text, optionOne, optionTwo} = question
     const {name, avatarURL} = author
 
+    console.log('q:' , question)
+
     return{
         id,
         name,
         avatar: avatarURL,
-        text,
+        text: optionOne.text,
         hasAnswered: (optionOne.votes.includes(authedUser) ||  optionTwo.votes.includes(authedUser)) ? true : false,
     }
 }
