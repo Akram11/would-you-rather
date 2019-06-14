425435446a0cbd3270e4fb8fbce7a7d568a65903
Akram11 <akram.f11@gmail.com>
2019-06-14 19:07:00 +0200

    reset text fields in new Question to empty after submit

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import {handleAddQuestion} from '../actions/questions'
+import { handleAddQuestion } from '../actions/questions'
 
 
 class NewQuestion extends Component {
@@ -27,11 +27,16 @@ class NewQuestion extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault()
-        const  optionOne = this.state.optionOne
-        const  optionTwo = this.state.optionTwo
-        const {dispatch} = this.props
-        console.log('options',optionOne, optionTwo)
+        const { optionOne, optionTwo } = this.state
+        const { dispatch } = this.props
         dispatch(handleAddQuestion(optionOne, optionTwo))
+
+        this.setState(() => ({
+            optionOne: '',
+            optionTwo: ''
+        }))
+
+        console.log("after dispatch",this.state)
     }
 
     render() {
@@ -41,9 +46,9 @@ class NewQuestion extends Component {
                 <h3>Would you rather...</h3>
                 <Form>
                     <Form.Group>
-                        <Form.Control size="lg" type="text" placeholder="first option" id="optionOne" onChange={this.handleChange} />
+                        <Form.Control size="lg" type="text" value={this.state.optionOne} placeholder="first option" id="optionOne" onChange={this.handleChange} />
                         <h4>OR</h4>
-                        <Form.Control size="lg" type="text" placeholder="second option" id="optionTwo" onChange={this.handleChange} />
+                        <Form.Control size="lg" type="text" value={this.state.optionTwo} placeholder="second option" id="optionTwo" onChange={this.handleChange} />
                         <Button variant="outline-secondary" size="lg" block onClick={this.handleSubmit}>Submit</Button>
                         <span> {this.state.option}</span>
                     </Form.Group>
