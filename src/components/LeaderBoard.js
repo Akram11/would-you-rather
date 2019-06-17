bf9e625c8a8d1e595631bbe5f6b5f44e6ccbe826
Akram11 <akram.f11@gmail.com>
2019-06-17 19:16:28 +0200

    update

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -6,14 +6,14 @@ class LeaderBoard extends Component {
   render() {
     const usersIDs = this.props.usersIDs;
     const users = this.props.users;
-    console.log(usersIDs);
+    console.log('props',usersIDs);
     console.log(this.props);
     return (
       <div className='container'>
         {/* {usersIDs.map( (id) => (<p>{ users[id].questions.length}</p> ))} */}
         {usersIDs.map(id => (
-            <li>
-                {id}
+            <li key='id'>
+                { users[id].name}
             <UserCard />
           </li>
         ))}
