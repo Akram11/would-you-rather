26e775002592f337cb301aa59de5c84dd6251a94
Akram11 <akram.f11@gmail.com>
2019-06-20 17:49:15 +0200

    added data to AnsweredQuesetion UI

--- src/components/AnsweredQuestion.js
+++ src/components/AnsweredQuestion.js
@@ -6,11 +6,22 @@ import Row from 'react-bootstrap/Row';
 import Media from 'react-bootstrap/Media';
 import ProgressBar from 'react-bootstrap/ProgressBar'
 import { connect } from "react-redux";
+import { Badge } from 'react-bootstrap'
+
+
 
 class AnsweredQuestion extends Component {
   render() {
     console.log(this.props)
-    const author = this.props.questions[this.props.id].author
+    const { authedUser, users, questions, id } = this.props
+    const authorID = questions[id].author
+    const authorName = users[authorID].name
+    const AuthedAnswer = users[authedUser].answers[id]
+    const OptionOneVotes = questions[id].optionOne.votes.length
+    const OptionTwoVotes = questions[id].optionTwo.votes.length
+    const totalVotes = OptionTwoVotes + OptionOneVotes
+
+    console.log('sfsf', AuthedAnswer, OptionOneVotes)
     return (
       <div className='container'>
         <div className='question'>
@@ -23,19 +34,19 @@ class AnsweredQuestion extends Component {
               alt='Generic placeholder'
             />
             <Media.Body>
-              Asked by {author}
+              Asked by {authorName}
               <h5> Would you rather?</h5>
-              result
+              results:
               <Form>
-                  <Form.Group as={Row}>
-                    <Form.Label as='legend' column sm={3} />
-                    <Col lg={1}>
-                    OptionOne
-                    <ProgressBar now={57} />
-                    Option Two
-                    <ProgressBar now={30} />
-                    </Col>
-                  </Form.Group>
+                <Form.Group as={Row}>
+                  <Form.Label as='legend' />
+                  <Col >
+                    <span>{questions[id].optionOne.text}</span>
+                    <ProgressBar max={totalVotes} now={OptionOneVotes * 10} label={`${OptionOneVotes} of ${totalVotes}`} />
+                    <span>{questions[id].optionTwo.text}</span>
+                    <ProgressBar max={totalVotes} now={OptionTwoVotes * 10} label={`${OptionTwoVotes} of ${totalVotes}`} />
+                  </Col>
+                </Form.Group>
               </Form>
             </Media.Body>
           </Media>
@@ -45,13 +56,14 @@ class AnsweredQuestion extends Component {
   }
 }
 
- function mapStateToProps({users, questions, authedUser}, props) {
-     const {id} = props.match.params
-     return {
-       id, 
-       questions,
-       users
-     }
+function mapStateToProps({ users, questions, authedUser }, props) {
+  const { id } = props.match.params
+  return {
+    id,
+    authedUser,
+    questions,
+    users
+  }
 
 }
 
