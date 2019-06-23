6abe333a8cf4215fcf0c94fb76714f87552da211
Akram A <akram.f11@gmail.com>
2019-06-23 14:38:45 +0200

    update users state when authedUser answers a question

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,14 +1,28 @@
-import { RECEIVE_USERS } from '../actions/users'
+import { RECEIVE_USERS } from '../actions/users';
+import { SAVE_QUESTION_ANSWER } from '../actions/questions';
 
 export default function users(state = {}, action) {
-    switch (action.type) {
-        case RECEIVE_USERS:
-            return {
-                ...state,
-                ...action.users
-            }
+  switch (action.type) {
+    case RECEIVE_USERS:
+      return {
+        ...state,
+        ...action.users
+      };
 
-        default:
-            return state
-    }
+    case SAVE_QUESTION_ANSWER:
+      const { authedUser, qid, answer } = action;
+      return {
+        ...state,
+        [authedUser]: {
+          ...state[authedUser],
+          answers: {
+            ...state[authedUser].answers,
+            [qid]: answer
+          }
+        }
+      };
+
+    default:
+      return state;
+  }
 }
