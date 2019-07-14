a71be28b68f6dbaa8ef013b23dc041ce4b79de91
Akram A <akram.f11@gmail.com>
2019-07-14 11:09:14 +0200

    changes to the routes file, keep user log in presistent

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,41 +1,61 @@
-import React, { Component } from "react";
-import { Switch, Route, withRouter, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { handleInitialData } from "../../actions/shared";
-import App from "../App";
-import Login from "../Login";
+import React, { Component } from 'react';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../../actions/shared';
+import App from '../App';
+import Login from '../Login';
+import NotFound from '../NotFound';
+import UnansweredQuestion from '../UnansweredQuestion';
 
 class Routes extends Component {
+  state = {
+    entryLocation: '',
+    firstTimeLoad: true
+  };
+
   componentDidMount() {
+    console.log('component did mount');
+    console.log('location:', this.props.location);
+    this.setState({ entryLocation: this.props.location.pathname });
     this.props.dispatch(handleInitialData());
     if (this.props.authedUser === null) {
-      this.props.history.push("/login");
+      this.props.history.push('/login');
+      console.log('in if');
     } else {
-      this.props.history.push("/");
+      console.log('in else');
+      this.props.history.push(this.sate.entryLocation);
     }
   }
 
-  Notfound = () => {
-    return <h1>Notfound</h1>;
-  };
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('did update');
+    console.log(prevProps, prevState);
+    if (
+      this.props.authedUser &&
+      this.state.firstTimeLoad &&
+      this.state.entryLocation
+    ) {
+      this.props.history.push(this.state.entryLocation);
+      this.setState({ firstTimeLoad: false });
+    }
+  }
 
   render() {
+    console.log('autherduser!!!!', this.props.authedUser);
+    console.log('state', this.state);
+    console.log('match', this.props.match);
     return (
       <>
         <Switch>
           <Route
-            exact
-            path="/"
-            render={() =>
-              this.props.authedUser === null ? (
-                <Redirect to="/login" />
-              ) : (
-                <App />
-              )
-            }
+            // exact
+            path='/'
+            render={() => (!this.props.authedUser ? <Login /> : <App />)}
           />
-          <Route path="/login" component={Login} />
-          <Route component={this.Notfound} />
+          {/*<Route path='/question/:id' component={UnansweredQuestion} />*/}
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/not-found' component={NotFound} />
+          {/*<Redirect to="/not-found" />*/}
         </Switch>
       </>
     );
